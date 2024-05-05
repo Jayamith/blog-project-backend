973a06d394d6c05526d4de9081552d532076176d
Jayamith Priyankara <jayamithpriyankara6@gmail.com>
2024-05-05 14:55:51 +0800

    user-model

---
+++ model/User/User.js
@@ -0,0 +1,104 @@
+const mongoose = require('mongoose');
+const { type } = require('os');
+
+//schema 
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ["user", "admin"],
+        default: ["user"]
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now(),
+    },
+    isVerified: {
+        type: String,
+        default: false,
+    },
+    accountLevel: {
+        type: String,
+        enum: ["bronze", "silver", "goled"],
+        default: "bronze",
+    },
+    profilePicture: {
+        type: String,
+        default: "",
+    },
+    coverImage: {
+        type: String,
+        default: "",
+    },
+    bio: {
+        type: String,
+    },
+    location: {
+        type: String,
+    },
+    notificationPreference: {
+        email: { type: String, default: true },
+        //..other notifications (sms)
+    },
+    gender: {
+        type: String,
+        enum: ["male", "female", "prefer not to say", "non-binary"],
+    },
+    profileViewers: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"
+    }],
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"
+    }],
+    blockedUsers: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "User"
+    }],
+    posts: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Post"
+    }],
+    likedPosts: [{
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: "Post"
+    }],
+    passwordResetToken: {
+        type: String,
+    },
+    passwordResetExpires: {
+        type: Date,
+    },
+    accountVerificationToken: {
+        type: String,
+    },
+    accountVerificationExpires: {
+        type: Date,
+    },
+}, {
+    timestamps: true,
+});
+
+//compile schema to model
+
+const user = mongoose.model("User", userSchema);
+
+module.exports = User;
--- server.js
+++ server.js
@@ -9,6 +9,6 @@ const app = express();
 const server = http.createServer(app);
 
 //? Start the server
-
 const PORT = process.env.PORT || 9080
+
 server.listen(PORT, console.log(`Server is running on port ${PORT}`));
